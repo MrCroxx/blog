9c260b8df2dffaf20b0670efb737ade4039ef00c
Meng Yao <mrcroxx@outlook.com>
2020-08-08 12:59:49 +0800

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 BASEDIR=$(cd $(dirname "${BASH_SOURCE[0]}") >/dev/null && pwd)
+
 cd ${BASEDIR}
+
+# UPDATE RAW BLOG
+
+git add .
+git commit -m "update"
+git push origin master
+
+# UPDATE GENERATED BLOG
+
 rm -r public/*
 hugo
 cd public
