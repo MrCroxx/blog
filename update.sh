aecbcef6a7295a91ca8928a9329da309770fdf82
MrCroxx <mrcroxx@outlook.com>
2021-01-21 14:11:45 +0800

    update

--- update.sh
+++ update.sh
@@ -13,6 +13,7 @@ git push origin master
 
 # UPDATE GENERATED BLOG
 # coding page
+cd ${BASEDIR}
 rm -r build/coding/*
 hugo --baseURL=https://blog.mrcroxx.com --destination=build/coding
 cd build/coding
@@ -21,6 +22,7 @@ git commit -m "update"
 git push origin master
 
 # github page
+cd ${BASEDIR}
 rm -r build/github/*
 hugo --baseURL=https://mrcroxx.github.io --destination=build/github
 cd build/github
