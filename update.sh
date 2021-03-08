a35f8cf3da70ae273db06d6d7772886809bfd954
MrCroxx <mrcroxx@outlook.com>
2021-03-08 12:12:38 +0800

    update

--- update.sh
+++ update.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
+# VARS
 BASEDIR=$(cd $(dirname "${BASH_SOURCE[0]}") >/dev/null && pwd)
 
-cd ${BASEDIR}
+# FUN
+UPDATE(){
+    cd ${BASEDIR}
 
-# UPDATE RAW BLOG
+    # UPDATE RAW BLOG
 
-git add .
-git commit -m "update"
-git push origin master
+    git add .
+    git commit -m "update"
+    git push origin master
 
 
-# UPDATE GENERATED BLOG
-# coding page
-cd ${BASEDIR}
-rm -r build/coding/*
-hugo --baseURL=https://blog.mrcroxx.com --destination=build/coding
-cd build/coding
-git add .
-git commit -m "update"
-git push origin master
+    # UPDATE GENERATED BLOG
+    # coding page
+    cd ${BASEDIR}
+    rm -r build/coding/*
+    hugo --baseURL=https://blog.mrcroxx.com --destination=build/coding
+    cd build/coding
+    git add .
+    git commit -m "update"
+    git push origin master
 
-# github page
-cd ${BASEDIR}
-rm -r build/github/*
-hugo --baseURL=https://mrcroxx.github.io --destination=build/github
-cd build/github
-git add .
-git commit -m "update"
-git push origin master
+    # github page
+    cd ${BASEDIR}
+    rm -r build/github/*
+    hugo --baseURL=https://mrcroxx.github.io --destination=build/github
+    cd build/github
+    git add .
+    git commit -m "update"
+    git push origin master
+}
+
+# ENTRY POINT
+while getopts "r" OPT; do
+    case $OPT in
+        r)
+        exit 0;;
+    esac
+done
+UPDATE
